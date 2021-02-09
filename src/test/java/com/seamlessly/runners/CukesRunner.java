34ec64436ad3864ba4c136dddd70c529c1a0defe
timecorner35 <adamimdj@gmail.com>
2021-02-09 13:17:34 +0000

    CukesRunner completed

--- src/test/java/com/seamlessly/runners/CukesRunner.java
+++ src/test/java/com/seamlessly/runners/CukesRunner.java
@@ -1,4 +1,18 @@
 package com.seamlessly.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/seamlessly/stepdefinitions",
+        dryRun = false,
+        tags = "@wip"
+)
 public class CukesRunner {
+
 }
